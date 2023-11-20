aa30171c825ff97e7747c7ef20a5199606fadc1e
Haaziq Jamal <121727878+Haaziq386@users.noreply.github.com>
2023-11-20 07:47:01 +0530

    Create globals.dart

---
+++ integrating_atlas/lib/globals.dart
@@ -0,0 +1,11 @@
+library my_prj.globals;
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+int hosfollowers = 5332;
+int hosvisitors = 10232;
+bool hosfollowed = false;
+int docfollowers = 701;
+int docvisitors = 1500;
+bool docfollowed = false;
